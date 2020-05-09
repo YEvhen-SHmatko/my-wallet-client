0ff8eee8adced1cbd2bcf3930b29d2712d4fe8c2
YEvhen-SHmatko <yevhen.shmatko@gmail.com>
2020-05-10 01:56:39 +0300

    create Title, edit git atributes add normalize add font roboto

--- src/components/LoginRegister/index.jsx
+++ src/components/LoginRegister/index.jsx
@@ -3,12 +3,13 @@ import Logo from './Logo';
 // import AuthFormWrap from './AuthFormWrap';
 // import Background from './Background';
 // import OtherIcon from './OtherIcon';
-// import Title from './Title';
+import Title from './Title';
 
 export default function LoginRegister() {
   return (
     <>
       <Logo />
+      <Title />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import './styles/normalize.css';
+import './fonts/Roboto/Roboto.css';
+import './styles/MyNormalize.css';
 
 ReactDOM.render(
   <React.StrictMode>
