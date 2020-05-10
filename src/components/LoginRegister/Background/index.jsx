cb28868d90e620ca2fd0530b2fe0c9d98c4a392b
YEvhen-SHmatko <yevhen.shmatko@gmail.com>
2020-05-10 14:57:59 +0300

    edit title, backgroun and LoginRegistration

---
+++ src/components/LoginRegister/Background/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Styles from './index.module.css';
+
+export default function index() {
+  return <section className={Styles.section} />;
+}
--- src/components/LoginRegister/index.jsx
+++ src/components/LoginRegister/index.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import Styles from './index.module.css';
 import Logo from './Logo';
 // import AuthFormWrap from './AuthFormWrap';
-// import Background from './Background';
+import Background from './Background';
 // import OtherIcon from './OtherIcon';
 import Title from './Title';
 
 export default function LoginRegister() {
   return (
     <>
-      <Logo />
-      <Title />
+      <header className={Styles.header}>
+        <Logo />
+      </header>
+      <main className={Styles.main}>
+        <Background />
+        <div className={Styles.section}>
+          <div className={Styles.first}>
+            <Title />
+          </div>
+          <div className={Styles.second}>
+            <div className={Styles.form}>form</div>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
