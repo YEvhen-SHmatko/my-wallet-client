f51218c84c8644e0aceeee66c7ffda5551ed5fa0
YEvhen-SHmatko <yevhen.shmatko@gmail.com>
2020-05-13 12:12:07 +0300

    create MonthStatic

---
+++ src/components/DashBoard/MonthStatistic/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Styles from './index.module.css';
+
+const index = ({ data }) => {
+  return (
+    <>
+      <ul className={Styles.list}>
+        <li className={Styles.item}>сводка</li>
+        {data &&
+          data.map(item => (
+            <li className={Styles.item} key={item.id}>
+              <span className={Styles.month}>{item.month}</span>
+              <span className={Styles.cost}>{item.cost}</span>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
+};
+
+index.propTypes = {
+  data: PropTypes.shape(
+    PropTypes.shape({
+      id: PropTypes.oneOfType(PropTypes.string, PropTypes.number),
+      month: PropTypes.string,
+      cost: PropTypes.string,
+    }),
+  ).isRequired,
+};
+export default index;
--- src/components/ExpensesSection/index.jsx
+++ src/components/ExpensesSection/index.jsx
@@ -5,8 +5,61 @@ import { Mobile, Default } from '../../services/media-query';
 import Calendar from '../DashBoard/Calendar';
 import MoneyForm from '../DashBoard/MoneyForm';
 import Table from '../DashBoard/Table';
+import MonthStatistic from '../DashBoard/MonthStatistic';
 import Styles from './index.module.css';
 
+const data = [
+  {
+    id: 1,
+    month: 'январь',
+    cost: '$532.90',
+  },
+  {
+    id: 2,
+    month: 'февраль',
+    cost: '$398.16',
+  },
+  {
+    id: 3,
+    month: 'март',
+    cost: '$323.94',
+  },
+  {
+    id: 4,
+    month: 'апрель',
+    cost: '$717.73',
+  },
+  {
+    id: 5,
+    month: 'май',
+    cost: '$254.42',
+  },
+  {
+    id: 6,
+    month: 'июнь',
+    cost: '$258.92',
+  },
+  {
+    id: 7,
+    month: 'июль',
+    cost: '$195.94',
+  },
+  {
+    id: 8,
+    month: 'август',
+    cost: '$144.88',
+  },
+  {
+    id: 9,
+    month: 'сентябрь',
+    cost: '$677.34',
+  },
+  {
+    id: 10,
+    month: 'октябрь',
+    cost: '$148.12',
+  },
+];
 export default class index extends Component {
   state = {};
 
@@ -21,7 +74,10 @@ export default class index extends Component {
             <Calendar />
             <MoneyForm />
           </div>
-          <Table />
+          <div className={Styles.main}>
+            <Table />
+            <MonthStatistic data={data} />
+          </div>
         </Default>
       </div>
     );
