72c078fae5007f5a3c706ef0c3a7eadaf9787863
YEvhen-SHmatko <yevhen.shmatko@gmail.com>
2020-05-24 09:54:05 +0300

    used mediaquery and react responsive in BalanceReport component

--- src/components/BalanceReport/index.jsx
+++ src/components/BalanceReport/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useMediaQuery } from 'react-responsive';
 import Styles from './index.module.css';
+import { isNotMobile } from '../../services/mediaQuery';
 
 const index = ({ value, date }) => {
+  const IsDefault = isNotMobile(useMediaQuery);
   return (
-    <div className={Styles.section}>
-      <div className={Styles.title}>{`Баланс на ${date}:`}</div>
-      <span className={Styles.value}>{`${value}.00 UAN`}</span>
+    <div className={IsDefault ? Styles.Default_section : Styles.section}>
+      <div
+        className={IsDefault ? Styles.Default_title : Styles.title}
+      >{`Баланс на ${date}:`}</div>
+      <span
+        className={IsDefault ? Styles.Default_value : Styles.value}
+      >{`${value}.00 UAN`}</span>
     </div>
   );
 };
