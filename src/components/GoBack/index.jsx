66d7289f0a12c09ae84dd74fc9b9095f6e7137a8
YEvhen-SHmatko <yevhen.shmatko@gmail.com>
2020-05-29 11:42:29 +0300

    fixed goBackButton

--- src/components/GoBack/index.jsx
+++ src/components/GoBack/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Styles from './index.module.css';
-import { Default } from '../../services/mediaQuery';
+import { Tablet, Desktop } from '../../services/mediaQuery';
 
 const Static = () => {
   return (
@@ -17,9 +17,12 @@ const Static = () => {
         <path d="M0 0h24v24H0z" fill="none" />
         <path d="M21 11H6.83l3.58-3.59L9 6l-6 6 6 6 1.41-1.41L6.83 13H21z" />
       </svg>
-      <Default>
+      <Tablet>
+        <span className={Styles.text}>Вернуться</span>
+      </Tablet>
+      <Desktop>
         <span className={Styles.text}>Вернуться на главную</span>
-      </Default>
+      </Desktop>
     </>
   );
 };
