b064f25e78b385a6b4be95c801d825725c217166
YEvhen-SHmatko <yevhen.shmatko@gmail.com>
2020-05-29 11:52:49 +0300

    fixed LoginRegister

--- src/components/LoginRegister/index.jsx
+++ src/components/LoginRegister/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useMediaQuery } from 'react-responsive';
+import { isMobile, isTablet } from '../../services/mediaQuery';
 import Styles from './index.module.css';
 import Header from '../Header';
 import AuthFormWrap from '../AuthFormWrap';
@@ -6,22 +8,49 @@ import Background from '../Background';
 import Title from '../Title';
 import Decoration from '../Decoration';
 
-export default function LoginRegister() {
+const LoginRegister = () => {
+  const IsMobile = isMobile(useMediaQuery);
+  const IsTablet = isTablet(useMediaQuery);
   return (
     <>
       <Header />
       <main className={Styles.main}>
         <Decoration />
         <Background />
-        <div className={Styles.section}>
-          <div className={Styles.first}>
+        <div
+          className={
+            IsMobile
+              ? Styles.Mobile_section
+              : IsTablet
+              ? Styles.Tablet_section
+              : Styles.Desktop_section
+          }
+        >
+          <div
+            className={
+              IsMobile
+                ? Styles.Mobile_first
+                : IsTablet
+                ? Styles.Tablet_first
+                : Styles.Desktop_first
+            }
+          >
             <Title />
           </div>
-          <div className={Styles.second}>
+          <div
+            className={
+              IsMobile
+                ? Styles.Mobile_second
+                : IsTablet
+                ? Styles.Tablet_second
+                : Styles.Desktop_second
+            }
+          >
             <AuthFormWrap />
           </div>
         </div>
       </main>
     </>
   );
-}
+};
+export default LoginRegister;
